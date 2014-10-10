40255327fd2238d2a16129a5e0819aae6dca7456
mode89 <mode89@mail.ru>
2014-10-10 22:08:18 +0800

    + header types_fwd.h

---
+++ include/ann/types_fwd.h
@@ -0,0 +1,14 @@
+#ifndef __ANN_TYPES_FWD_H__
+#define __ANN_TYPES_FWD_H__
+
+#include "cmn/shared_ptr_typedefs.h"
+#include <memory>
+
+namespace ANN {
+
+    SHARED_PTR_FORWARD_TYPEDEFS( INetwork );
+    SHARED_PTR_FORWARD_TYPEDEFS( Perceptron );
+
+} // namespace ANN
+
+#endif // __ANN_TYPES_FWD_H__
