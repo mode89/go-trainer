3fa06ac8ccec3a1ab5adb4f4eb8d9eef866be1fc
mode89 <mode89@mail.ru>
2015-02-26 10:30:20 +0800

    migrated main.cpp to opennn

--- trainer/main.cpp
+++ trainer/main.cpp
@@ -4,48 +4,70 @@
 #include "gnugo/player.h"
 #include "gnugo/player_ann.h"
 #include "gnugo/player_random.h"
+#include "opennn/mathematical_model.h"
+#include "opennn/neural_network.h"
+#include "opennn/performance_functional.h"
+#include "opennn/training_strategy.h"
 
 #include <fstream>
+
+using namespace OpenNN;
 
 const unsigned kBoardSize       = 9;
 const unsigned kCellCount       = kBoardSize * kBoardSize;
 const unsigned kPopulationSize  = 10;
-const unsigned kGameCount       = 50;
+const unsigned kGameCount       = 25;
 
-double FitnessOp( ANN::ConstPerceptronIn nw )
+class Objective : public PerformanceTerm
 {
-    double fitness = 0.0;
-
-    #pragma omp parallel
-    #pragma omp for
-    for ( int i = 0; i < kGameCount; ++ i )
+public:
+    double calculate_performance( const Vector<double> & params ) const
     {
-        gnugo::Engine       engine( 1, kBoardSize );
-        gnugo::PlayerAnn    blackPlayer( nw, engine );
-        gnugo::Player       whitePlayer( engine );
-        gnugo::Game         game( kBoardSize, blackPlayer, whitePlayer, engine );
-        game.Play();
+        NeuralNetwork nw( *neural_network_pointer );
+        nw.set_parameters( params );
 
-        fitness += engine.GetScore( go::COLOR_WHITE );
+        double score = 0.0;
+
+        #pragma omp parallel
+        #pragma omp for
+        for ( int i = 0; i < kGameCount; ++ i )
+        {
+            gnugo::Engine       engine( 1, kBoardSize );
+            gnugo::PlayerAnn    blackPlayer( nw, engine );
+            gnugo::Player       whitePlayer( engine );
+            gnugo::Game         game( kBoardSize, blackPlayer, whitePlayer, engine );
+            game.Play();
+
+            score += engine.GetScore( go::COLOR_WHITE );
+        }
+
+        return score / kGameCount;
     }
 
-    return fitness / kGameCount;
-}
+    double calculate_performance() const
+    {
+        return 0.0;
+    }
+};
 
 int main( int argc, char * argv[] )
 {
-    ANN::IPerceptronGeneticAlgorithmTrainerRef trainer =
-        ANN::CreatePerceptronGeneticAlgorithmTrainer( FitnessOp, kCellCount, kCellCount + 1, kPopulationSize );
-    trainer->SetMutationProbability( 0.001f );
-    trainer->SetMutationSpeed( 0.3f );
+    NeuralNetwork nw( kCellCount, kCellCount, kCellCount + 1 );
 
-    double fitness = 0.0;
-    do {
-        fitness = trainer->Step();
-        CMN_MSG( "%3.3f", fitness );
+    Objective objective;
+    objective.set_neural_network_pointer( &nw );
 
-        std::ofstream fittestOutputStream( "fittest.nw", std::ios::out | std::ios::trunc );
-        boost::archive::binary_oarchive fittestOutputArchive( fittestOutputStream );
-        fittestOutputArchive << *trainer->GetFittest();
-    } while ( fitness > -70.0 );
+    PerformanceFunctional pf( &nw );
+    pf.set_objective_type( PerformanceFunctional::USER_OBJECTIVE );
+    pf.set_user_objective_pointer( &objective );
+
+    TrainingStrategy trainingStrategy( &pf );
+    trainingStrategy.set_initialization_type( TrainingStrategy::EVOLUTIONARY_ALGORITHM );
+    trainingStrategy.set_main_type( TrainingStrategy::NO_MAIN );
+
+    EvolutionaryAlgorithm * evolutionaryAlgorithm = trainingStrategy.get_evolutionary_algorithm_pointer();
+    evolutionaryAlgorithm->set_population_size( kPopulationSize );
+    evolutionaryAlgorithm->set_display_period( 1 );
+
+    trainingStrategy.perform_training();
 }
